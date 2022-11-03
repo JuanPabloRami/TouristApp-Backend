c7361157dc200980ab0e65825a9acb082102d287
lSebastianGomezl <cupmi009@gmail.com>
2022-11-03 01:33:14 -0500

    Se aggrega los modelos de app al admin

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Negocio, Tipo_Negocio, Departamento, Ciudad , Item, Comentario
 # Register your models here.
+
+admin.site.register(Negocio)
+admin.site.register(Tipo_Negocio)
+admin.site.register(Departamento)
+admin.site.register(Ciudad)
+admin.site.register(Item)
+admin.site.register(Comentario)
+
